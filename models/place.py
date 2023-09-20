a60e330cedfd074412ae9e80a06d581b43262f2f
Menna Hassan <h.menna85@gmail.com>
2023-09-21 00:05:43 +0300

    place fixed

--- models/place.py
+++ models/place.py
@@ -8,13 +8,11 @@ from os import getenv
 from models.amenity import Amenity
 from models.review import Review
 
-place_amenity = Table(
-    'place_amenity', Base.metadata,
-    Column('place_id', String(60), ForeignKey('places.id'),
-           primary_key=True, nullable=False),
-    Column('amenity_id', String(60), ForeignKey('amenities.id'),
-           primary_key=True, nullable=False),
-)
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False),)
 
 
 class Place(BaseModel, Base):
@@ -38,6 +36,7 @@ class Place(BaseModel, Base):
                              viewonly=False, overlaps="place_amenities")
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
+
         @property
         def reviews(self):
             """get linked reviews"""
