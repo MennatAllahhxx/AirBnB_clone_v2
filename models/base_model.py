d0d79c8a1c83eee26a37143e0009476fde71c8c7
farah-halim <level2bis2@gmail.com>
2023-09-20 20:53:42 +0000

    fix base model

--- models/base_model.py
+++ models/base_model.py
@@ -12,15 +12,14 @@ class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), unique=True,
                 nullable=False, primary_key=True)
-    created_at = Column(DateTime, nullable=False,
-                        default=datetime.utcnow())
-    updated_at = Column(DateTime, nullable=False,
-                        default=datetime.utcnow())
+    created_at = Column(
+        DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(
+        DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
@@ -39,12 +38,14 @@ class BaseModel:
 
     def __str__(self):
         """Returns a string representation of the instance"""
-        return '[{}] ({}) {}'.format(
-            self.__class__.__name__,
-            self.id, self.__dict__)
+        new_dict = self.__dict__.copy()
+        if '_sa_instance_state' in new_dict:
+            del new_dict['_sa_instance_state']
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, new_dict)
 
     def save(self):
-        """Updates updated_at with current time when instance is changed"""
+        """Updates updated_at with curren time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
         storage.new(self)
