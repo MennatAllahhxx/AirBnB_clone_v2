b7232064d4f29e7f674098cb3d6c1eb688bd8a5b
Menna Hassan <h.menna85@gmail.com>
2023-10-23 02:53:40 +0300

    updated

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -18,10 +18,15 @@ def states():
 @app.route("/states/<id>", strict_slashes=False)
 def state_id(id):
     """a fun to display state by id"""
+    obj = None
+    notfound = True
     for state in storage.all(State).values():
         if state.id == id:
-            return render_template('9-states.html', state=state)
-    return render_template('9-states.html')
+            obj = state
+            notfound = False
+            break
+    return render_template('9-states.html', id=id, state=obj,
+                           notfound=notfound)
 
 
 @app.teardown_appcontext
