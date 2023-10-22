b30b490c3c1d3e8b86e912887053be9465ffdecf
Menna Hassan <h.menna85@gmail.com>
2023-10-23 02:08:40 +0300

    updated

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -12,7 +12,7 @@ app = Flask(__name__)
 def states_list():
     """a fun to display states and cities"""
     states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+    return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
