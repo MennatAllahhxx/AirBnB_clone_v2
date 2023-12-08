43e678e0f81664620b67e659d6905c3ad3b70109
Menna Hassan <h.menna85@gmail.com>
2023-12-08 04:58:11 +0200

    updated

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+a Fabric script that generates a .tgz archive 
+"""
+
+from facric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """a method to generate .tgz archive"""
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"versions/web_static_{date}.tgz"
+    try:
+        local(f"tar -cvzf {filename} web_static")
+        return filename
+    except Exception as e:
+        return None
