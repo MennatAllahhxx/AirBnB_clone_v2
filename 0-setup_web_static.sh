4c345861b32bcf6e77214ea3c53c0c9169f923c3
Menna Hassan <h.menna85@gmail.com>
2023-12-08 04:42:52 +0200

    updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #a Bash script that sets up your web servers for the deployment of web_static
 sudo apt-get update -y
-sudo apt-install nginx -y
+sudo apt-get install nginx -y
 
 sudo mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
 sudo sh -c 'echo "hi ;)" > /data/web_static/releases/test/index.html'
@@ -11,10 +11,10 @@ sudo chown -R ubuntu:ubuntu /data/
 
 sudo sh -c 'echo "server {
     listen 80;
-    listen [::]:80 defaultserver;
+    listen [::]:80 default_server;
     server_name mennatallah.tech;
     location /hbnb_static {
-        alias /data/web_static/current;
+        alias /data/web_static/current/;
     }
 }" > /etc/nginx/sites-available/default'
 
